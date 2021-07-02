1cbbbd6cdc22169fe82a9eb3be437e3e7a663d1a
dennismarfo <dennismarfo@gmail.com>
2021-07-02 07:27:56 +0200

    ajout de multer

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
+const auth = require('../middleware/auth');
 
-router.post('/', sauceCtrl.createSauce);
-router.put('/:id', sauceCtrl.updateSauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
-router.get('/:id', sauceCtrl.getOneSauce);
-router.get('/', sauceCtrl.getAllSauces)
+router.post('/', auth, sauceCtrl.createSauce);
+router.put('/:id', auth, sauceCtrl.updateSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces)
 
 
 
