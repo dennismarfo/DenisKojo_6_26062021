8c7bd62af558c7a63df93d48d16603dea57a4bcb
dennismarfo <dennismarfo@gmail.com>
2021-07-04 02:36:32 +0200

    modification routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
@@ -8,7 +9,8 @@ const userRoutes = require('./routes/user');
 
 
 mongoose.connect('mongodb+srv://dennis_marfo:RTNrnFedXKWt4F5@cluster0.u26h2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
+  { useCreateIndex: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
@@ -25,7 +27,9 @@ app.use((req, res, next) => {
 // Methode qui va transformer le corps de la requête en objet json
 app.use(bodyParser.json());
 
-app.use('/api/sauce', sauceRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const sauceCtrl = require('../controllers/sauce');
+const sauceCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, sauceCtrl.createSauce);
-router.put('/:id', auth, sauceCtrl.updateSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.updateSauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces)
